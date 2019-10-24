39f74a7c45b21eb06c0ec77bc9b88930e865a787
Muhammed Shafi <shafikulambil@gmail.com>
2019-10-24 07:59:30 +0530

    Add files via upload

---
+++ this.js
@@ -0,0 +1,33 @@
+console.log('Hello World');
+// (function init() {
+//     console.log('Init...');
+
+//     var btn = document.createElement('button');
+//     btn.addEventListener('click', function(){
+//         if(true){
+//             addContent();
+//         }
+//     });
+//     btn.textContent = 'Add 0';
+//     btn.classList.add('btn', 'btn-primary', 'shadow-none');
+//     document.body.appendChild(btn);
+
+// })();
+
+// function addContent() {
+//     var counter = ((this.textContent) ? Number.parseInt(this.textContent.split(" ")[1]) : 0) + 1;
+//     this.textContent = 'Add ' + counter;
+
+// }
+   
+
+
+
+
+
+
+
+
+
+
+
